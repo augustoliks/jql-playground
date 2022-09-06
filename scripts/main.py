b08d85156bb580f6dcb21a031a678c82f000411b
Carlos Neto <carlos.neto.dev@gmail.com>
2022-09-05 23:26:38 -0300

    add automation

--- scripts/main.py
+++ scripts/main.py
@@ -3,39 +3,7 @@ from requests.auth import HTTPBasicAuth
 import json
 from pprint import pprint
 
-email = "carlos.neto.dev@gmail.com"
-jira_token = ""
-# q = "issue in addedToSprintAfterStart('2', 'OPS Sprint 1')"
-# q = "Sprint = 'OPS Sprint 1' AND issue not in addedToSprintAfterStart('2', 'OPS Sprint 1')"
-# q = "Sprint = 'OPS Sprint 1'"
 
-
-base = "https://liks.atlassian.net"
-
-# url = "https://liks.atlassian.net/rest/api/3/search?jql=filter=10003"
-# a = q.replace(' ', '+')
-#
-# url = f"{base}/rest/agile/1.0/board"
-# url = f"{base}/rest/api/2/search?jql={a}"
-# url = f"{base}/rest/api/3/issue/OPS-1"
-url = f"{base}/rest/api/3/search?jql=issueFunction+in+addedAfterSprintStart('2','OPS Sprint 1')"
-
-
-
-
-auth = HTTPBasicAuth(email, jira_token)
-
-headers = {
-   "Accept": "application/json",
-   "Content-Type": "application/json"
-}
-
-response = requests.request(
-   "GET",
-
-   url,
-   headers=headers,
-   auth=auth
-)
-
-print(json.dumps(response.json()))
+credentials = ("carlos.neto.dev@gmail.com", "")
+jira = JIRA(url, basic_auth=credentials)
+get_property = jira.issue_property("OPS-10", "ground")
